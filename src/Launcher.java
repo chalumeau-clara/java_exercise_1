447575caf70c516073415892200d98641396fc77
clara.chalumeau <clara.chalumeau@epita.fr>
2022-02-09 17:21:37 +0100

    ex3

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,9 +4,15 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Welcome");
         Scanner console = new Scanner(System.in);
-        String name = console.nextLine();
-        if (!name.equals("quit")) {
-            System.out.println("Unknown command");
+        while (true)
+        {
+            String name = console.nextLine();
+            if (name.equals("quit"))
+                break;
+            else {
+                System.out.println("Unknown command");
+            }
         }
+
     }
 }

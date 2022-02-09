3b0982adbdde4c8498846527b6adb5d60df058f8
clara.chalumeau <clara.chalumeau@epita.fr>
2022-02-09 22:39:50 +0100

    partie 5

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,10 @@
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Launcher {
 
@@ -14,7 +20,24 @@ public class Launcher {
         return pres;
     }
 
-    public static void main(String[] args) {
+    public static void freq(String str){
+        str = str.replaceAll("[^A-Za-z0-9]+", " ");
+        str = str.toLowerCase(Locale.ROOT);
+        List<String> arr = Arrays.asList(str.split(" "));
+
+        if (str.isBlank()){
+            return;
+        }
+        var freqMap = arr.stream()
+                .collect(Collectors.groupingBy(s->s, Collectors.counting()));
+
+        List<String> li = null;
+        var max3 = freqMap.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed()).limit(3);
+        String l = max3.map(Map.Entry::getKey).collect(Collectors.joining(" "));
+        System.out.printf(l + "\n");
+    }
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Welcome");
         Scanner console = new Scanner(System.in);
         while (true)
@@ -29,9 +52,24 @@ public class Launcher {
                 System.out.println(res);
                 String i = console.nextLine();
             }
+            else if (name.equals("freq")) {
+                System.out.printf("Enter a path\n");
+                String path = console.nextLine();
+                Path filepath = Paths.get(path);
+
+                try {
+                    String content = Files.readString(filepath);
+                    freq(content);
+                }
+                catch (IOException e) {
+                    System.out.printf("Unreadable file: ");
+                    e.printStackTrace();
+                }
+            }
             else {
                 System.out.println("Unknown command");
             }
+            //console.close();
         }
 
     }
